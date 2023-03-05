be0760985d9033c2504b29f70c8af617b1e0a849
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-03-05 21:49:34 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,19 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title(string='Cronometru tip Pomodoro')
+window.config(padx=100, pady=50, bg=RED)
+
+canvas = Canvas(width=200, height=223, bg=RED, highlightthickness=0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(100, 110, image=tomato_img)
+canvas.create_text(100, 114, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='blue')
+
+canvas.pack()
+
+
+
+
+window.mainloop()
